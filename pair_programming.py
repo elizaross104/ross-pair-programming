86efd6e85ef6b73534f01a73dbeb79cc39dc41d9
Eliza Ross <elizaross@mines.edu>
2024-02-22 10:33:25 -0700

    Add notebook ready for feedback from partner

---
+++ pair_programming.py
@@ -0,0 +1,16 @@
+# Author: Eliza Ross
+# Feb. 22, 2024
+# Pair programming assignment
+
+# Editor: 
+
+
+# Write a function that takes two values (feet and inches) and converts to meters
+
+def convert_meters(array): #input should be array with format [feet, inches]
+    feet = array[0] #pulls feet value from the 0 index of the input array
+    inches = array[1] #pulls inches value from the 1st index of the input array
+    decimal = inches/12.0 #converts inches to feet by dividing by 12
+    total_feet = feet + decimal #adds the value for feet to the value of inches converted to feet to find total feet
+    meters = total_feet / 3.281 #converts total feet to meters, 3.281 found from online calculator
+    return meters #returns number of meters
